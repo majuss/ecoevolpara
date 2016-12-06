571f2285ee337c1f4f59fb0ec9f5d1d864b44bfa
majuss <biomajuss@gmail.com>
2016-12-06 13:15:34 +0100

    modified seafile install script

--- latest/docs/source/appendix/scripts/install_seafile_client.sh
+++ latest/docs/source/appendix/scripts/install_seafile_client.sh
@@ -36,49 +36,71 @@ get_login_password(){
 	read -p "Enter your seafile login password: " login_password
 }
 
-get_local_sync(){
-	read -p "Enter local directory you want to sync: " local_dir
+get_local_dir(){
+	read -p "Enter local directory you want to sync: (/home/marius for example)" local_dir
 }
 #################functions end###########
 
 #################variable initiation###
 username="$SUDO_USER"
+ignore_link="https://raw.githubusercontent.com/majuss/ecoevolpara/master/latest/docs/source/appendix/scripts/seafile-ignore.txt"
 #################variable initiation end###
 
 
 
 while true; do
-    read -p "Which client do you want to install? graphical[1] or commandline[2]: " client_type
+    read -p "Which client do you want to install? graphical[1] or commandline[2]. Or hit [3] to uninstall any previous installed client " client_type
     case $client_type in
         [1]* )	add_repo
-				echo "installing gui client" #aptitude install -y seafile-gui
+				aptitude install -y seafile-gui
 				get_valid_username
-
-				echo "creating dir for user $username" #mkdir /home/seafile /home/seafile/"$username" /etc/seafile /etc/seafile/$username
-				echo "owning dir" #chown $username:$username /home/seafile/"$username" /etc/seafile/$username
-				echo "exporting env_var" #export CCNET_CONF_DIR=/etc/seafile/$username
-				
+				mkdir /home/seafile /home/seafile/"$username" /etc/seafile /etc/seafile/$username
+				chown $username:$username /home/seafile/"$username" /etc/seafile/$username
+				export CCNET_CONF_DIR=/etc/seafile/$username
+				wget $ignore_link -O /home/"$username"/seafile-ignore.txt
 				break;;
 
 
         [2]* )	add_repo
-        		echo "installing cli client" #aptitude install -y seafile-cli
+        		aptitude install -y seafile-cli
         		get_valid_username
         		get_login_email
         		get_login_password
-        		get_local_sync
+        		get_local_dir
         		get_library_id
-        		echo "creating dir for user $username" #mkdir /home/seafile /home/seafile/"$username" /etc/seafile /etc/seafile/$username
-				echo "owning dir" #chown $username:$username /home/seafile/"$username" /etc/seafile/$username
-				echo "exporting env_var" #export CCNET_CONF_DIR=/etc/seafile/$username
-				echo "wget ignore list" #wget ignore list -O /home/$username
-        		sudo -u $username seaf-cli init -c /etc/seafile_confs/$username -d /home/seafile/$username
+        		mkdir /home/seafile /home/seafile/"$username" /etc/seafile /etc/seafile/$username /usr/local/bin/seafile_startup
+				chown $username:$username /home/seafile/"$username" /etc/seafile/$username
+				export CCNET_CONF_DIR=/etc/seafile/$username
+				wget ignore list -O /home/$username
+        		sudo -u $username seaf-cli init -c /etc/seafile/$username -d /home/seafile/$username
         		sudo -u $username seaf-cli start -c /etc/seafile/$username
-        		sudo -u $username seaf-cli sync -l $library_id -s https://svalbard.biologie.hu-berlin.de -d /home/$username -c /etc/seafile/$username -u $login_email -p login_password
+        		sudo -u $username seaf-cli sync -l $library_id -s https://svalbard.biologie.hu-berlin.de -d $local_dir -c /etc/seafile/$username -u $login_email -p login_password
+        		echo "#!/bin/sh; seaf-cli start -c /etc/seafile/"$username"; sleep 2; seaf-cli sync -l "$library_id" -s https://svalbard.biologie.hu-berlin.de -d "$local_dir" -c /etc/seafile/"$username" -u "$login_email" -p "$login_password"" >> /usr/local/bin/seafile_startup/start_"$username.sh"
+				chown $username:$username /usr/local/bin/seafile_startup/start_"$username.sh"
+				#sudo -u $username echo ""
+
         		
 
 
         		break;;
-        * ) echo "Please answer 1 or 2.";;
+
+        [3]* )	get_valid_username
+				aptitude purge -y seafile-cli seafile-gui
+				rm /usr/local/bin/seafile_startup/$username
+				rm /etc/seafile/$username
+				rm /home/seafile/$username
+
+
+
+
+				break;;
+        * ) echo "Please answer 1, 2 or 3.";;
     esac
 done
+
+
+
+
+
+#line="@reboot  /var/lib/seafile-client/seaf-cli-launcher"
+#(crontab -l; echo "$line" ) | uniq | crontab -
